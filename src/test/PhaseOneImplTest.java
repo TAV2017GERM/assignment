8f1266c5fab242ff4444357ad987e415250c0424
CYuZeric <chyj1993@outlook.com>
2017-01-30 11:03:16 +0100

    JUnit set up

---
+++ src/test/PhaseOneImplTest.java
@@ -0,0 +1,83 @@
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* PhaseOneImpl Tester. 
+* 
+* @author GeoffreyC
+* @since <pre>Jan 30, 2017</pre> 
+* @version 1.0 
+*/ 
+public class PhaseOneImplTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: MoveForward() 
+* 
+*/ 
+@Test
+public void testMoveForward() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: MoveBackward() 
+* 
+*/ 
+@Test
+public void testMoveBackward() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: Park() 
+* 
+*/ 
+@Test
+public void testPark() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: unPark() 
+* 
+*/ 
+@Test
+public void testUnPark() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: Wherels() 
+* 
+*/ 
+@Test
+public void testWherels() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: isEmpty() 
+* 
+*/ 
+@Test
+public void testIsEmpty() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
