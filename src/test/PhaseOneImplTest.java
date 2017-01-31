18a39499bb1df5b47e8b14379089f1f5aff7ae5d
CYuZeric <chyj1993@outlook.com>
2017-01-31 13:21:36 +0100

    testMoveForward step 1

--- src/test/PhaseOneImplTest.java
+++ src/test/PhaseOneImplTest.java
@@ -1,90 +1,79 @@
-import implementation.PhaseOne;
-import implementation.PhaseOneImpl;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.Before; 
-import org.junit.After; 
 
-/** 
-* implementation.PhaseOneImpl Tester.
-* 
-* @author GeoffreyC
-* @since <pre>Jan 30, 2017</pre> 
-* @version 1.0 
-*/ 
-public class PhaseOneImplTest { 
+import implementation.PhaseOneImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * implementation.PhaseOneImpl Tester.
+ *
+ * @author GeoffreyC
+ * @version 1.0
+ * @since <pre>Jan 30, 2017</pre>
+ */
+public class PhaseOneImplTest {
 
     PhaseOneImpl phaseOne;
 
-@Before
-public void before() throws Exception {
-    phaseOne = new PhaseOneImpl();
-} 
+    @Before
+    public void before() throws Exception {
+        phaseOne = new PhaseOneImpl();
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-/** 
-* 
-* Method: MoveForward() 
-* 
-*/ 
-@Test
-public void testMoveForward() throws Exception { 
+    /**
+     * Method: MoveForward()
+     */
+    @Test
+    public void testMoveForward() throws Exception {
+        int i = phaseOne.whereIs();
+        phaseOne.moveForward();
+        Assert.assertEquals(phaseOne.whereIs(), i + 1);
+    }
+
+    /**
+     * Method: MoveBackward()
+     */
+    @Test
+    public void testMoveBackward() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: Park()
+     */
+    @Test
+    public void testPark() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: unPark()
+     */
+    @Test
+    public void testUnPark() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: Wherels()
+     */
+    @Test
+    public void testWhereIs() throws Exception {
+        Assert.assertEquals(phaseOne.whereIs(), 0);
+    }
+
+    /**
+     * Method: isEmpty()
+     */
+    @Test
+    public void testIsEmpty() throws Exception {
 //TODO: Test goes here...
-
-} 
-
-/** 
-* 
-* Method: MoveBackward() 
-* 
-*/ 
-@Test
-public void testMoveBackward() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: Park() 
-* 
-*/ 
-@Test
-public void testPark() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: unPark() 
-* 
-*/ 
-@Test
-public void testUnPark() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: Wherels() 
-* 
-*/ 
-@Test
-public void testWhereIs() throws Exception {
-    Assert.assertEquals(phaseOne.whereIs(), 0);
-} 
-
-/** 
-* 
-* Method: isEmpty() 
-* 
-*/ 
-@Test
-public void testIsEmpty() throws Exception { 
-//TODO: Test goes here...
-} 
+    }
 
 
 } 
