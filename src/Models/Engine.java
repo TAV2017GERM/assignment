f0cda2ad066282095c3fdd957174afa22bcecabb
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-02-13 22:38:04 +0100

    adding interfaces and classes for phase 2

---
+++ src/Models/Engine.java
@@ -0,0 +1,21 @@
+package Models;
+
+/**
+ * @author Group 4 on 2/13/17.
+ */
+public class Engine implements ActuatorsInterface{
+    @Override
+    public void runFwd() {
+
+    }
+
+    @Override
+    public void runBkd() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ src/Models/UltraSonic.java
@@ -0,0 +1,16 @@
+package Models;
+
+/**
+ * @author Group 4 on 2/13/17.
+ */
+public class UltraSonic implements USInterface{
+    @Override
+    public void measureDistance() {
+
+    }
+
+    @Override
+    public int getDistance() {
+        return 0;
+    }
+}
