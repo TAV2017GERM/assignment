ef0500ec4efc5d7ce898f31ab4fb1976483bec02
Mattias Landkvist <mattias.landkvist@gmail.com>
2017-02-15 10:25:38 +0100

    CarStatusTest structure

---
+++ src/test/Models/CarStatusTest.java
@@ -0,0 +1,33 @@
+package Models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Group4
+ * @version 1.0
+ * @since <pre>Feb 15, 2017</pre>
+ */
+public class CarStatusTest {
+    @Test
+    public void setCarPosition() throws Exception {
+
+    }
+
+    @Test
+    public void whereIs() throws Exception {
+
+    }
+
+    @Test
+    public void registerParkingPlaces() throws Exception {
+
+    }
+
+    @Test
+    public void fetchParkingPlace() throws Exception {
+
+    }
+
+}
