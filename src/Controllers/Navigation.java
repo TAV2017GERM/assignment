ce8c53d16a13b590d297f0c1f39096c086a8c72c
Mattias Landkvist <mattias.landkvist@gmail.com>
2017-02-15 12:41:05 +0100

    Navigation rename stuff

--- src/Controllers/Navigation.java
+++ src/Controllers/Navigation.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * @author by Group4 on 2017-01-27.
  */
-public class Controller implements ControllerInterface {
+public class Navigation implements NavigationInterface {
     private int POSITION = 0;
     private int IS_EMPTY_COUNTER = 0;
 
