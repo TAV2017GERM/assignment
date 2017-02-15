d656369cf947a147b009e68ab3bc82428ebeab60
geoffrey <chyj1993@gmail.com>
2017-02-15 13:38:10 +0100

    fix

--- src/Models/Actuators.java
+++ src/Models/Actuators.java
@@ -3,20 +3,7 @@ package Models;
 /**
  * @author Group 4 on 2/13/17.
  */
-public class Actuators implements ActuatorsInterface{
-
-
-
-
-
-
-
-
-
-
-
-
-
+public class Actuators implements ActuatorsInterface {
 
 
     @Override
