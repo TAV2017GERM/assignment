dffd64a625ca402d9d8d3205aab49218b836702f
geoffrey <chyj1993@gmail.com>
2017-02-15 10:43:26 +0100

    mockito setup

--- src/test/Models/UltraSonicTest.java
+++ src/test/Models/UltraSonicTest.java
@@ -1,7 +1,7 @@
 package Models;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
