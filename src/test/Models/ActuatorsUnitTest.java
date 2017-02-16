fcc686614f2e38d8d8682ccb1b90a1ace77e3b73
geoffrey <chyj1993@gmail.com>
2017-02-16 12:05:14 +0100

    UnitTest

--- src/test/Models/ActuatorsUnitTest.java
+++ src/test/Models/ActuatorsUnitTest.java
@@ -1,11 +1,11 @@
 package Models;
 
-import junit.framework.AssertionFailedError;
+
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-
-import java.util.IllegalFormatCodePointException;
+import org.junit.rules.ExpectedException;
 
 
 /**
@@ -17,24 +17,34 @@ import java.util.IllegalFormatCodePointException;
  */
 public class ActuatorsUnitTest {
     private Actuators actuator;
+
     @Before
     public void before() throws Exception {
-      actuator = new Actuators();
+        actuator = new Actuators();
     }
 
-    @Test
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test(expected = IllegalArgumentException.class)
     public void testMoveForward() {
         int pos = 0;
-        for(int i = 0; i<500;i++){
+        for (int i = 0; i < 500; i++) {
             pos = actuator.moveForward(pos);
 
         }
 
+        Assert.assertEquals(500, actuator.moveForward(pos));
+
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testMoveBackward() {
+        int pos = 0;
+        pos = actuator.moveBackward(pos);
+        pos = actuator.moveForward(pos);
 
+        Assert.assertEquals(0, actuator.moveBackward(pos));
     }
 
 }
