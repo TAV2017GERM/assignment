e5d7d371586809d63b310b572d274ee3b405e882
geoffrey <chyj1993@gmail.com>
2017-02-16 11:00:01 +0100

    UnitTest

---
+++ src/test/Models/ActuatorsUnitTest.java
@@ -0,0 +1,21 @@
+package Models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by geoffrey on 2017-02-16.
+ */
+class ActuatorsUnitTest {
+    @Test
+    void moveForward() {
+
+    }
+
+    @Test
+    void moveBackward() {
+
+    }
+
+}
